37f70f37a0845085c86f4d07547629c8a0869855
DalOpez <alex-grazhdankin@mail.ru>
2025-04-29 13:04:35 +0300

    print 5 last products

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Product
 
 # Create your views here.
 def home(request):
+    last_products = Product.objects.order_by('created_at')[:5]
+    for product in last_products:
+        print(product)
     return render(request, 'catalog/home.html')
 
 def contacts(request):
